cf9cfef3d0cb5102fa0172f45193a80c7ec4ff9b
gloria <gloria.gao115@gmail.com>
2022-12-22 21:13:55 +0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const functionToString = require("sinon/lib/sinon/util/core/function-to-string")
+
+
+function writeCards(arr, messages) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(`Thank you, ${arr[i]}, for the wonderful ${messages} gift!`)
+  }
+  return newArr
+}
+
+writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise')
+console.log(newArr)
+
+function countDown(x) {
+  for (let i = x; i >= 0; i--) {
+    console.log(i)
+  }
+}
+countDown(10)
+
